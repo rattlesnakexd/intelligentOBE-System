dce4251af0eb96627f92734d84594cc1fc5969cb
Abdullah Sohail <abdullah.sohail577@gmail.com>
2023-04-27 23:23:41 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import Login from './login/Login';
 import AdminHome from './AdminHome/AdminHome';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Login ></Login>
+      <AdminHome></AdminHome>
     
     </div>
   );
