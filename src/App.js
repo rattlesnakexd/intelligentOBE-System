fc991b0a597eb85c70586d42a5473e7e579ae999
Abdullah Sohail <abdullah.sohail577@gmail.com>
2023-10-13 22:53:06 +0500

    Login Form updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import AdminHome from "./Pages/AdminHome/AdminHome";
 import GenerateResults from "./Pages/GenerateResults/GenerateResults";
 import Section from "./Pages/section/section";
@@ -8,18 +8,27 @@ import UploadSheet from "./Pages/UploadSheet/UploadSheet";
 import Login from "./Pages/login/Login";
 import "./App.css";
 
-
 function App() {
+  const role = localStorage.getItem('role');
+  
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/master-sheet" element={<AdminHome />} />
-          <Route path="/section-sheet" element={<Section/>} />
-          <Route path="/generate-sheets" element={<TeacherHome />} />
-          <Route path="/progress-sheet" element={<UploadSheet/>} />
-          <Route path="/generate-results" element={<GenerateResults/>}/>
-          <Route path="/" element={<Login />} />
+          {role === 'admin' && (
+            <>
+              <Route path="/master-sheet" element={<AdminHome />} />
+              <Route path="/section-sheet" element={<Section />} />
+            </>
+          )}
+          {role === 'teacher' && (
+            <>
+              <Route path="/generate-sheets" element={<TeacherHome />} />
+              <Route path="/progress-sheet" element={<UploadSheet />} />
+              <Route path="/generate-results" element={<GenerateResults />} />
+            </>
+          )}
+          <Route path="/" element={<Login />}/>
         </Routes>
       </div>
     </Router>
--- src/Pages/login/Login.js
+++ src/Pages/login/Login.js
@@ -1,46 +1,78 @@
-import React, {useState} from 'react';
-import './Login.css'
+import React, { useState } from 'react';
+import './Login.css';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { TextField, Button } from '@mui/material';
 
-function Login(){
+function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
-  const HandleLogin = async () => {
-    if (username === "abdullah"){
-        if (password === "abdullah.123"){
-            navigate("/master-sheet", {state: {name:'Abdullah'}}); 
-        }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.get(`http://127.0.0.1:8000/authentication/get_user/?username=${username}&password=${password}`);
+
+      if (response.data) {
+        console.log(response.data);
+        localStorage.setItem('role', response.data.role);
+
+        setTimeout(() => {
+          if (localStorage.getItem('role') === "admin") {
+            navigate('/master-sheet');
+          } else {
+            navigate('/generate-sheets');
+          }
+        }, 100);
+      } 
+    } catch (error) {
+      console.error('Error:', error);
     }
-    else if (username === "ahmad"){
-        if (password ==="ahmad.123"){
-            navigate("/generate-sheets", {state: {name:'Ahmad'}}); 
-        }
-    }
-    
   }
-    return(
-        <div className='login-container'>
-            
 
-            <div className='login-left'>
-                <div className='credentials'>
-                <h1>OBE-System</h1>
-                <form onSubmit={HandleLogin}>
-                <input type='text' value={username} placeholder='Enter Username' onChange={(e) => setUsername(e.target.value)}></input><br></br>
-                <input type='password' value={password} placeholder='Enter Password' onChange={(e) => setPassword(e.target.value)}></input><br></br>
-                <button className='login-btn' type='submit'>Login</button>
-                </form>
-                </div>
-                
-            </div>
-            <div className='login-right'>
-                <img className='login-img' src='images/showcase.gif' alt="Imagehere" width="100%" height="100%"></img>
-
-            </div>
-
+  return (
+    <div className='login-container'>
+      <div className='login-left'>
+        <div className='credentials'>
+          <h1>OBE-System</h1>
+          <form onSubmit={handleSubmit} style={{display: "flex", flexDirection: "column"}}>
+          <TextField 
+              type='text' 
+              label='Username' 
+              value={username} 
+              onChange={(e) => setUsername(e.target.value)} 
+              required 
+              style={{ marginBottom: '10px' }} // Add some margin
+              size='small'
+            />
+            <TextField 
+              type='password' 
+              label='Password' 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)} 
+              required 
+              style={{ marginBottom: '10px' }} // Add some margin
+              size='small'
+            />
+            <Button 
+              className='login-btn' 
+              type='submit' 
+              variant='contained' 
+              color='primary' 
+              fullWidth={false} // No need for fullWidth
+            >
+              Login
+            </Button>
+          </form>
         </div>
-    );
+      </div>
+      <div className='login-right'>
+        <img className='login-img' src='images/showcase.gif' alt="Imagehere" width="100%" height="100%" />
+      </div>
+    </div>
+  );
 }
 
 export default Login;
